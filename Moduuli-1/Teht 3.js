016cdc8631f254c53f8d71d2044f6589853a56f1
joelraut <joelraut@metropolia.fi>
2022-11-11 20:06:01 +0200

    update

--- Moduuli-1/Teht 3.js
+++ Moduuli-1/Teht 3.js
@@ -1,1 +1,10 @@
-'use strict'
+'use strict';
+
+
+var numero1 = prompt('Syötä numero 1');
+var numero2= prompt('Syötä numero 2');
+var numero3 = prompt('Syötä numero 3');
+
+const v1 = numero1 + numero2 + numero3
+
+const target = document.getElementById('kohde').innerHTML+= `<li>${v1}</li>`;
