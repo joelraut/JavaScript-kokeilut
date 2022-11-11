37df4f6b1c936f9794015eaa14d42f9bf6bd792a
joelraut <joelraut@metropolia.fi>
2022-11-11 22:04:13 +0200

    update

--- Moduuli-1/Teht 3.js
+++ Moduuli-1/Teht 3.js
@@ -4,7 +4,16 @@
 var numero1 = prompt('Syötä numero 1');
 var numero2= prompt('Syötä numero 2');
 var numero3 = prompt('Syötä numero 3');
+const nmr1 = parseInt(numero1);
+const nmr2 = parseInt(numero2);
+const nmr3 = parseInt(numero3);
 
-const v1 = numero1 + numero2 + numero3
+const v1 = nmr1 + nmr2 + nmr3
+const v2 = (nmr1 + nmr2 + nmr3) / 3
+const v3 = nmr1 * nmr2 * nmr3
 
-const target = document.getElementById('kohde').innerHTML+= `<li>${v1}</li>`;
+const target = document.getElementById('sum').innerHTML+= `<li>Summa:${v1}</li>`;
+const target1 = document.getElementById('keski').innerHTML+= `<li>Keskiarvo: ${v2}</li>`;
+const target2= document.getElementById('tulo').innerHTML+= `<li>Tulo:${v1}</li>`;
+
+
