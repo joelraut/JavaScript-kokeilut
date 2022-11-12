73229fd58a4c2b58e22a398944d2f85e22f83680
joelraut <joelraut@metropolia.fi>
2022-11-12 13:29:20 +0200

    update

--- Moduuli-1/Teht 3.js
+++ Moduuli-1/Teht 3.js
@@ -8,12 +8,12 @@ const nmr1 = parseInt(numero1);
 const nmr2 = parseInt(numero2);
 const nmr3 = parseInt(numero3);
 
-const v1 = nmr1 + nmr2 + nmr3
-const v2 = (nmr1 + nmr2 + nmr3) / 3
-const v3 = nmr1 * nmr2 * nmr3
+const v1 = nmr1 + nmr2 + nmr3;
+const v2 = (nmr1 + nmr2 + nmr3) / 3;
+const v3 = nmr1 * nmr2 * nmr3;
 
-const target = document.getElementById('sum').innerHTML+= `<li>Summa:${v1}</li>`;
+const target = document.getElementById('sum').innerHTML+= `<li>Summa: ${v1}</li>`;
 const target1 = document.getElementById('keski').innerHTML+= `<li>Keskiarvo: ${v2}</li>`;
-const target2= document.getElementById('tulo').innerHTML+= `<li>Tulo:${v1}</li>`;
+const target2= document.getElementById('tulo').innerHTML+= `<li>Tulo: ${v3}</li>`;
 
 
---
+++ Moduuli-1/Teht 4.js
@@ -0,0 +1,11 @@
+'use strict'
+const name = prompt('What is your name?')
+const mumber = Math.random();
+
+if (number % 2 == 0) {
+            console.log('You entered an even number');
+        }
+        else {
+            console.log('You entered an odd number.');
+        }
+
