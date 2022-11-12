d4f113f8aa7d2227e2660342a1693153a5611a71
joelraut <joelraut@metropolia.fi>
2022-11-12 17:45:53 +0200

    update

--- Moduuli-1/Teht 4.js
+++ Moduuli-1/Teht 4.js
@@ -1,6 +1,7 @@
 'use strict'
 const name = prompt('What is your name?')
-const mumber = Math.random();
+
+const mumber = Math.random(1, 4);
 
 if (number % 2 == 0) {
             console.log('You entered an even number');
