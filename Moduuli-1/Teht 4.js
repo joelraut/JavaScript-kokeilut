eb663bb45eb994e97e2fb38fff29d83420bdcfaa
joelraut <joelraut@metropolia.fi>
2022-11-13 16:31:30 +0200

    update

--- Moduuli-1/Teht 4.js
+++ Moduuli-1/Teht 4.js
@@ -8,18 +8,18 @@ if (number === 0) {
   document.getElementById('0').innerHTML+= `<li>${name}, you are in  ${Gryffindor}</li>`;
 }
 else if (number === 1) {
-  const Gryffindor = 'Gryffindor'
-  document.getElementById('0').innerHTML+= `<li>${name}, you are in  ${Gryffindor}</li>`;
+  const Slytherin = 'Slytherin'
+  document.getElementById('1').innerHTML+= `<li>${name}, you are in  ${Slytherin}</li>`;
 
 }
 else if (number === 2) {
-  const Gryffindor = 'Gryffindor'
-  document.getElementById('0').innerHTML+= `<li>${name}, you are in  ${Gryffindor}</li>`;
+  const Hufflepuff = 'Hufflepuff'
+  document.getElementById('2').innerHTML+= `<li>${name}, you are in  ${Hufflepuff}</li>`;
 
 }
 else if (number === 3) {
-  const Gryffindor = 'Gryffindor'
-  document.getElementById('0').innerHTML+= `<li>${name}, you are in  ${Gryffindor}</li>`;
+  const Ravenclaw = 'Ravenclaw'
+  document.getElementById('3').innerHTML+= `<li>${name}, you are in  ${Ravenclaw}</li>`;
 
 }
 
