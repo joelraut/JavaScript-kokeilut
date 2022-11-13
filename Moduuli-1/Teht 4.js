c7584df42c277fd08ae8963e217318218698caa7
joelraut <joelraut@metropolia.fi>
2022-11-13 18:34:34 +0200

    Joe

--- Moduuli-1/Teht 4.js
+++ Moduuli-1/Teht 4.js
@@ -1,26 +1,26 @@
 'use strict'
 const name = prompt('What is your name?')
 
- const number = Math.random() * 3;
+const numero = Math.random() * 3;
 
-if (number === 0) {
+if (numero=== 0) {
   const Gryffindor = 'Gryffindor'
   const gri = document.getElementById('0')
   gri.innerHTML = `<li>${name}, you are in  ${Gryffindor}</li>`;
 }
-else if (number === 1) {
+else if (numero === 1) {
   const Slytherin = 'Slytherin'
   const sly = document.getElementById('1')
   sly.innerHTML = `<li>${name}, you are in  ${Slytherin}</li>`;
 
 }
-else if (number === 2) {
+else if (numero === 2) {
   const Hufflepuff = 'Hufflepuff'
   const huf = document.getElementById('2')
   huf.innerHTML = `<li>${name}, you are in  ${Hufflepuff}</li>`;
 
 }
-else if (number === 3) {
+else if (numero === 3) {
   const Ravenclaw = 'Ravenclaw'
   const rav = document.getElementById('3')
   rav.innerHTML = `<li>${name}, you are in  ${Ravenclaw}</li>`;
