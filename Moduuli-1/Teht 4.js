d2add01d35f15b35b2cf0ed028a7f3ac88472dba
joelraut <joelraut@metropolia.fi>
2022-11-13 16:59:28 +0200

    update

--- Moduuli-1/Teht 4.js
+++ Moduuli-1/Teht 4.js
@@ -5,21 +5,25 @@ const number = Math.random() * 3;
 
 if (number === 0) {
   const Gryffindor = 'Gryffindor'
-  document.getElementById('0').innerHTML+= `<li>${name}, you are in  ${Gryffindor}</li>`;
+  const gri = document.getElementById('0')
+  gri.innerHTML = `<li>${name}, you are in  ${Gryffindor}</li>`;
 }
 else if (number === 1) {
   const Slytherin = 'Slytherin'
-  document.getElementById('1').innerHTML+= `<li>${name}, you are in  ${Slytherin}</li>`;
+  const sly = document.getElementById('1')
+  sly.innerHTML = `<li>${name}, you are in  ${Slytherin}</li>`;
 
 }
 else if (number === 2) {
   const Hufflepuff = 'Hufflepuff'
-  document.getElementById('2').innerHTML+= `<li>${name}, you are in  ${Hufflepuff}</li>`;
+  const huf = document.getElementById('2')
+  huf.innerHTML = `<li>${name}, you are in  ${Hufflepuff}</li>`;
 
 }
 else if (number === 3) {
   const Ravenclaw = 'Ravenclaw'
-  document.getElementById('3').innerHTML+= `<li>${name}, you are in  ${Ravenclaw}</li>`;
+  const rav = document.getElementById('3')
+  rav.innerHTML = `<li>${name}, you are in  ${Ravenclaw}</li>`;
 
 }
 
