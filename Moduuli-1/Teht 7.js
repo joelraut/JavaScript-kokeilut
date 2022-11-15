f1e8459d5a72e205f54a0a15a2d47024572088f6
joelraut <joelraut@metropolia.fi>
2022-11-15 22:32:48 +0200

    jee

--- Moduuli-1/Teht 7.js
+++ Moduuli-1/Teht 7.js
@@ -1,1 +1,17 @@
 'use strict'
+const dice = prompt('How many times would you like to roll the dice');
+
+var rolls = []
+for (let i = 1; i < dice; i++){
+  var d1 = Math.floor(Math.random() * 6) + 1;
+    rolls.push(d1)
+
+}
+
+let sum = 0
+for (const value of rolls) {
+  sum += value;
+}
+
+ const yes1 = document.getElementById('yes1');
+  yes1.innerHTML = `<li>Result: ${sum}</li>`;
