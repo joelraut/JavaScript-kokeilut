6cdb27c4ca11402a8696593071bbd95a13001dcd
joelraut <joelraut@metropolia.fi>
2022-12-03 16:38:52 +0200

    jee

--- Moduuli-4/4b.js
+++ Moduuli-4/4b.js
@@ -4,13 +4,51 @@ const apiUrl = 'https://api.tvmaze.com/search/shows?q=';
 
 lomake.addEventListener('submit', async function(evt) {
   evt.preventDefault();
-  const hakusana =document.querySelector('#query').value;
+  const hakusana = document.querySelector('#query').value;
 
   const vastaus = await fetch(apiUrl + hakusana);
-  if(!vastaus.ok) {
+  if (!vastaus.ok) {
     throw new Error(vastaus.statusText)
   }
 
-  const sarjat  = await vastaus.json();
-  console.log(sarjat);
+  const sarjat = await vastaus.json();
+  console.log(sarjat)
+  const nimi = sarjat.map(element => {
+    return `
+<div class ="kuva">
+<article>
+ <figure> 
+ <img scr="${element.show.image.medium} alt= 'kuva' "> 
+ </figure>
+  </article>
+  </div>`;
+
+  }).join("");
+  console.log(nimi)
+
+  document
+  .querySelector("#name")
+  .insertAdjacentHTML("afterbegin", nimi);
+
+
+//   const nimi = sarjat.map(element => element.show.name);
+//
+//   const linkki = sarjat.map(element => element.show.url);
+//
+//   const kuva = sarjat.map(element => element.show.image);
+//
+//   const summary = sarjat.map(element => element.show.summary);
+//
+//   console.log(nimi, linkki, kuva, summary)
+//
+//
+// for (let i in nimi) {
+//   let figure = document.querySelector("#figure");
+//   let element = document.createElement("figure");
+//   element.innerHTML = nimi[i];
+//   nimi.append(element);
+// }
+
 });
+
+
