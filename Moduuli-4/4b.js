5456ce43961ea03b0e0d6f12ee38c49718fc6d6e
joelraut <joelraut@metropolia.fi>
2022-12-07 19:45:54 +0200

    jee

--- Moduuli-4/4b.js
+++ Moduuli-4/4b.js
@@ -1,67 +1,66 @@
 'use strict';
-const lomake = document.querySelector('form');
-const apiUrl = 'https://api.tvmaze.com/search/shows?q=';
+const form = document.querySelector('form');
+const section = document.querySelector('section');
 
-lomake.addEventListener('submit', async function(evt) {
-
-  evt.preventDefault();
-  const hakusana = document.querySelector('#query').value;
-
-  const vastaus = await fetch(apiUrl + hakusana);
-  if (!vastaus.ok) {
-    throw new Error(vastaus.statusText)
+async function fetchjson(url, options = {}){
+  if (!answer.ok){
+    throw new Error(answer.statusText);
   }
-  location.reload();
-  const sarjat = await vastaus.json();
-console.log(sarjat)
-  const nimi = sarjat.map(element => {
-    return `
+  return await answer.json()
+}
+
+form.addEventListener('submit', async function(evt) {
+  console.clear()
+  section.innerText= '';
+  evt.preventDefault();
+  const q = document.querySelector('#query').value;
+  const search = 'https://api.tvmaze.com/search/shows?q='+ q;
+  const json = await fetchjson(search);
+
+  for (let i in json){
+  const article = document.createElement('article');
+  const img = document.createElement('img');
+  const genres = document.createElement('p')
+  const button = document.createElement('button');
+  let title = json[i]['show']['name'];
+  const link = json[i]['show']['id'];
+
+  (json[i]['show']['genres'].length === 0) ?
+      genres.innerHTML = '(No genres listed)':
+      genres.append(json[i]['show']['genres'].join('/'));
+
+  if (json)[i]['show']['image'] === null) {
+    img.src ='https://via.placeholder.com/210x295?text=no+image';
+}   else if (json)[i]['show']['image']['medium'] === null){
+    img.src ='https://via.placeholder.com/210x295?text=no+image';
+}   else {
+    img.src = (json)[i]['show']['image']['medium'];
+    }
+    img.alt='picture';
+
+    article.innerHTML += `<h2>${title}</h2>`;
+    button.append('info modal');
+    const iframe = document.querySelector('iframe');
+    const dialog = document.querySelector('dialog');
+    const span = document.querySelector('span');
+
+    button.addEventListener('click', function(evt){
+      evt.preventDefault();
+    dialog.showModal();
+    iframe.scr = json [i]['show']['url'];
+    });
+    span.addEventListener('click, fucntion(evt') {
+      evt.preventDefault();
+      dialog.close()
+    }
+  })
 
 
 
 
-<article>
-<figure>
 
-<img class="kuva1" src="${element.show.image.medium}">
-<figcaption>${element.show.name}</figcaption>
- </figure>
- 
- <p>${element.show.summary}</p>
-  </article>
-
- 
-  `;
-
-  }).join("");
-
-
-  document
-  .querySelector("#name")
-  .insertAdjacentHTML("afterbegin", nimi);
+  }
 
 
 
 
-
-
-});
-
-
-//   const nimi = sarjat.map(element => element.show.name);
-//
-//   const linkki = sarjat.map(element => element.show.url);
-//
-//   const kuva = sarjat.map(element => element.show.image);
-//
-//   const summary = sarjat.map(element => element.show.summary);
-//
-//   console.log(nimi, linkki, kuva, summary)
-//
-//
-// for (let i in nimi) {
-//   let figure = document.querySelector("#figure");
-//   let element = document.createElement("figure");
-//   element.innerHTML = nimi[i];
-//   nimi.append(element);
-// }
